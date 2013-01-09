472f8e6e12d008c43dc3b27deef825bfbd0fb2cd
T.MURACHI <murachi@er-keiyo.sakura.ne.jp>
2013-01-10 08:26:10 +0900

    * 時刻入力コントロール自体は概ね完成。 * コントロールを利用するサンプルプログラムを作成中…。

---
+++ wpf/TimeInputSample/TimeInputModel/NoteBook.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Data;
+using System.Data.SqlServerCe;
+
+namespace TimeInputModel
+{
+    public class NoteBook
+    {
+        public class DateTimeNote
+        {
+            public int Code { get; private set; }
+            public DateTime NoteDateTime { get; private set; }
+            public string NoteDetail { get; private set; }
+
+            internal DateTimeNote(int code, DateTime date_time, string detail)
+            {
+                Code = code;
+                NoteDateTime = date_time;
+                NoteDetail = detail;
+            }
+        }
+
+        public class NoteCollection : IEnumerable
+        {
+            SqlCeCommand sql_command;
+
+            internal NoteCollection(SqlCeCommand command)
+            {
+                sql_command = command;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                var reader = sql_command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var note = new DateTimeNote((int)reader["NOTE_CODE"],
+                        (DateTime)reader["NOTE_DATETIME"], reader["NOTE_DETAIL"] as string);
+
+                    yield return note;
+                }
+            }
+        }
+
+        SqlCeConnection connection = new SqlCeConnection("Data Source = DateTimeNotes.sdf");
+
+        private NoteBook()
+        {
+            connection.Open();
+        }
+
+        private NoteCollection ActiveData
+        {
+            get
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "select * from NOTES_TABLE";
+                return new NoteCollection(command);
+            }
+        }
+
+        private void Insert(DateTimeNote note)
+        {
+        }
+
+        private static NoteBook singleton = new NoteBook();
+
+        public static NoteCollection GetActiveData()
+        {
+            return singleton.ActiveData;
+        }
+
+        public static int InsertNote(DateTime date_time, string detail)
+        {
+
+        }
+    }
+}
--- wpf/TimeInputSample/TimeInputSample/ViewModel/ViewModelBase.cs
+++ wpf/TimeInputSample/TimeInputSample/ViewModel/ViewModelBase.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace TimeInputSample.ViewModel
 {
-    public class ViewModelBase
+    /// <summary>
+    /// View Model クラスの基底クラス。
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
     {
+        /// <summary>
+        /// プロパティが変更されましたイベント。
+        /// </summary>
+        /// <remarks>
+        /// こいつから派生するクラスのインスタンスが View にバインドされると、
+        /// View の表示を更新する処理がこのイベントに勝手に関連づけられるらしいですよ。
+        /// </remarks>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// プロパティの変更を通知する。
+        /// </summary>
+        /// <param name="property_name">プロパティ名</param>
+        /// <remarks>
+        /// 指定したプロパティ名のプロパティをバインドした表示属性の表示を更新したいときに
+        /// こいつを呼べばいい、らしいですよ。
+        /// </remarks>
+        protected void NotifyPropertyChanged(string property_name)
+        {
+            if (PropertyChanged == null) return;
+            PropertyChanged(this, new PropertyChangedEventArgs(property_name));
+        }
     }
 }

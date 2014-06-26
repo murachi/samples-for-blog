f8939041c14a491e5ab6c355dcf76021e2acf768
T.MURACHI (村山 俊之) <murachi@er-keiyo.sakura.ne.jp>
2014-06-26 17:32:08 +0900

    オブザーバーパターンのサンプル作成中…

---
+++ c++/api-design-for-c++/ch03-pattern/observer/observer.h
@@ -0,0 +1,21 @@
+/**
+	@file	observer.h
+	@brief	オブザーバーオブジェクトの抽象クラス宣言
+**/
+#ifndef APIDES_OBSERVER_H
+#define APIDES_OBSERVER_H
+
+#include <boost/noncopyable.hpp>
+
+namespace apides {
+
+class ObserverBase : private boost::noncopyable {
+public:
+	virtual ~IObserver() = default;
+	virtual void Update() = 0;
+};
+
+}	//namespace apides
+
+#endif	//APIDES_OBSERVER_H
+

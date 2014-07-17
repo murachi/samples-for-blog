490b4fd5826dd8c1108d161e418b1177f0b9759b
T.MURACHI (村山 俊之) <murachi@er-keiyo.sakura.ne.jp>
2014-07-18 00:24:48 +0900

    ラムダを使った値変更コールバックが可能かどうかを検証するミニマムケースを作成中…。

---
+++ c++/minimal-cases/value-modifier-lambda/data.h
@@ -0,0 +1,47 @@
+/**
+	@file	data.h
+	@brief	データオブジェクトクラス宣言
+*/
+#ifndef MINIMUM_DATA_H
+#define MINIMUM_DATA_H
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <boost/noncopyable.hpp>
+
+namespace minimum {
+
+class Data : private boost::noncopyable {
+	struct Impl;
+	std::unique_ptr<Impl> impl;
+
+public:
+	Data();
+	~Data();
+
+	void appendKey(std::string const& key, std::string const& value = std::string{});
+	void dropKey(std::string const& key);
+
+	std::string const& operator[](std::string const& key) const;
+
+	struct Detail {
+		std::vector<std::string> keys;
+		std::vector<std::string> values;
+	};
+
+	template <typename ModFunc>
+	Data & modify(ModFunc func);
+
+	struct DetailReference {
+		std::vector<std::string> & keys;
+		std::vector<std::string> & values;
+	};
+	typedef void (*UpdateCallBack)(DetailReference const&);
+
+	Data & setUpdateCallBack(UpdateCallBack callback);
+};
+
+}	//namespace minimum
+
+#endif	//MINIMUM_DATA_H
---
+++ c++/minimal-cases/value-modifier-lambda/main.cpp
@@ -0,0 +1,39 @@
+/**
+	@file	main.cpp
+	@brief	メインルーチン
+*/
+
+#include "data.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	minimum::Data data{};
+	data.setUpdateCallBack([](minimum::Data::DetailReference const& detail){
+		for (decltype(detail.keys.size()) i = 0; i < detail.keys.size(); i++)
+			std::cout << detail.keys[i] << " : " << detail.values[i] << std::endl;
+		std::cout << std::endl;
+	});
+
+	data.appendKey("name", "村山 俊之");
+	data.appendKey("role", "社長");
+	data.appendKey("age", "36");
+
+	data.dropKey("age");
+
+	std::string birth{"1978/2/7"};
+	std::string company{"株式会社はらぺこ"};
+
+	data.modify([birth, company](minimum::Data::Detail & detail){
+		detail.keys.erase(detail.keys.begin() + 1);
+		auto role = detail.values[1];
+		detail.values.erase(detail.values.begin() + 1);
+		detail.keys.push_back("birth");
+		detail.values.push_back(birth);
+		detail.keys.push_back("company");
+		detail.values.push_back(company);
+	});
+
+	return 0;
+}

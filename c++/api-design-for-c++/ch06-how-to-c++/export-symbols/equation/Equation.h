529215a4f942d2dd1ea2550b6161e5800ac0913e
村山 俊之 <murachi@er-keiyo.sakura.ne.jp>
2015-06-01 21:46:33 +0900

    * N次方程式クラス作成中…。

--- c++/api-design-for-c++/ch06-how-to-c++/export-symbols/equation/Equation.h
+++ c++/api-design-for-c++/ch06-how-to-c++/export-symbols/equation/Equation.h
@@ -1,5 +1,6 @@
 /**
-	@file	
+	@file	Equation.h
+	@brief	N次方程式クラス定義
 */
 
 #ifndef INCLUDE_APIDES_EQUATION_EQUATION_H
@@ -8,17 +9,29 @@
 #include <dll-export.h>
 
 #include <memory>
+#include <iterator>
 #include <vector>
 
 namespace equation {
 
+/**
+	@brief	N次方程式クラス
+*/
 DLL_PUBLIC
 class Equation {
 	class Impl;
 	std::unique_ptr<Equation::Impl> impl;
 
+	Equation() = delete;
+	Equation(Equation const&) = delete;
+	Equation(Equation &&) = delete;
+	Equation & operator=(Equation const&) = delete;
+	Equation & operator=(Equation &&) = delete;
+
 public:
-	
+	Equation(const double [] coeffs, size_t size);
+	Equation(std::const_iterator<double> coeff_st, std::const_iterator<double> coeff_ed);
+	~Equation();
 };
 
 } // namespace equation

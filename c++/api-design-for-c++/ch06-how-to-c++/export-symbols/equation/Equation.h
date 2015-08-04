8a9c99e49df318b9f8daaa1a734fe7416ea9efcd
村山 俊之 <murachi@er-keiyo.sakura.ne.jp>
2015-08-04 21:45:53 +0900

    * タブは全てスペースに置き換えた。  * メソッドをいくつか追加。

--- c++/api-design-for-c++/ch06-how-to-c++/export-symbols/equation/Equation.h
+++ c++/api-design-for-c++/ch06-how-to-c++/export-symbols/equation/Equation.h
@@ -1,6 +1,6 @@
 /**
-	@file	Equation.h
-	@brief	N次方程式クラス定義
+    @file   Equation.h
+    @brief  N次方程式クラス定義
 */
 
 #ifndef INCLUDE_APIDES_EQUATION_EQUATION_H
@@ -15,23 +15,29 @@
 namespace equation {
 
 /**
-	@brief	N次方程式クラス
+    @brief  N次方程式クラス
 */
 DLL_PUBLIC
 class Equation {
-	class Impl;
-	std::unique_ptr<Equation::Impl> impl;
+    class Impl;
+    std::unique_ptr<Equation::Impl> impl;
 
-	Equation() = delete;
-	Equation(Equation const&) = delete;
-	Equation(Equation &&) = delete;
-	Equation & operator=(Equation const&) = delete;
-	Equation & operator=(Equation &&) = delete;
+    Equation() = delete;
+    Equation(Equation const&) = delete;
+    Equation(Equation &&) = delete;
+    Equation & operator=(Equation const&) = delete;
+    Equation & operator=(Equation &&) = delete;
 
 public:
-	Equation(const double [] coeffs, size_t size);
-	Equation(std::const_iterator<double> coeff_st, std::const_iterator<double> coeff_ed);
-	~Equation();
+    Equation(const double [] coeffs, size_t size);
+    Equation(std::const_iterator<double> coeff_st, std::const_iterator<double> coeff_ed);
+    ~Equation();
+
+    double calc(double x) const;
+
+    void setBegin(double x);
+    void setStep(double step);
+    double calcNext();
 };
 
 } // namespace equation

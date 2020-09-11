e054704a4adef8206fa32f192e7cddc213001271
Murayama Toshiyuki (T.MURACHI) <harapekomurachi@gmail.com>
2020-09-11 17:15:54 +0900

    issue #1 - async したタスクを並列実行する方法

--- rust/thread_sample/src/main.rs
+++ rust/thread_sample/src/main.rs
@@ -1,7 +1,8 @@
 use std::thread;
 use std::time::Duration;
-use futures::future::{self, Future};
-use futures::executor;
+use futures::future::Future;
+use futures::executor::ThreadPool;
+use std::io::Read;
 
 fn make_task(num: i32, dur: u64) -> impl Future<Output = ()> {
     async move {
@@ -19,33 +20,16 @@ fn main() {
     //let h2 = thread::spawn(make_task(2, 2_000_u64));
     //let h3 = thread::spawn(make_task(3, 1_500_u64));
 
-    //let h1 = async {
-    //    let mut dur_total = 0_u64;
-    //    for i in 0..10 {
-    //        println!("thread #1 count {} ({}ms).", i, dur_total);
-    //        thread::sleep(Duration::from_millis(1_000_u64));
-    //        dur_total += 1_000_u64;
-    //    }
-    //};
-    //let h2 = async {
-    //    let mut dur_total = 0_u64;
-    //    for i in 0..10 {
-    //        println!("thread #2 count {} ({}ms).", i, dur_total);
-    //        thread::sleep(Duration::from_millis(1_500_u64));
-    //        dur_total += 1_500_u64;
-    //    }
-    //};
+    let pool = ThreadPool::new().expect("create thread pool failed.");
 
     println!("please wait.");
-    let joined_task = future::join_all(vec![
-        make_task(1, 1_000_u64),
-        make_task(2, 2_000_u64),
-        make_task(3, 1_500_u64)]);
-    //let joined_task = future::join3(make_task(1, 1_000_u64), make_task(2, 2_000_u64), make_task(3, 1_500_u64));
-    //let joined_task = future::join(h1, h2);
-    executor::block_on(joined_task);
+    pool.spawn_ok(make_task(1, 1_000_u64));
+    pool.spawn_ok(make_task(2, 2_000_u64));
+    pool.spawn_ok(make_task(3, 1_500_u64));
     //h1.join().expect("thread h1 join failed.");
     //h2.join().expect("thread h2 join failed.");
     //h3.join().expect("thread h3 join failed.");
+    println!("press any key.");
+    std::io::stdin().read(&mut [0]).unwrap();
     println!("program end.");
 }

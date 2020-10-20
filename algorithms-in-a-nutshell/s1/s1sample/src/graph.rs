47da647890f1726adcb8d344f3ff95c7bf3fef08
Murayama Toshiyuki (T.MURACHI) <harapekomurachi@gmail.com>
2020-10-20 21:23:54 +0900

    issue #2 - sec.1 凸包計算サンプルプログラム作成中…。

---
+++ algorithms-in-a-nutshell/s1/s1sample/src/graph.rs
@@ -0,0 +1,14 @@
+use svg::Document;
+use svg::node::element::{Polygon, Circle, Group}
+
+pub struct Graph {
+  points: Vec<f64>,
+  polygon: Vec<f64>,
+}
+
+impl Graph {
+  pub fn new(usize point_num) -> Graph {
+      let mut points = Vec::with_capacity(point_num);
+
+  }
+}

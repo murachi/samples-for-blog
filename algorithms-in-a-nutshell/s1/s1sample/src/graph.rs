b905e3fe3960c2290d2db9dab6f72b1c2f41026f
Murayama Toshiyuki <harapekomurachi@gmail.com>
2020-10-20 23:51:06 +0900

    issue #2 - sec.1 凸包計算サンプルプログラム作成中…。

--- algorithms-in-a-nutshell/s1/s1sample/src/graph.rs
+++ algorithms-in-a-nutshell/s1/s1sample/src/graph.rs
@@ -1,5 +1,6 @@
 use svg::Document;
 use svg::node::element::{Polygon, Circle, Group}
+use rand::prelude::*;
 
 pub struct Graph {
   points: Vec<f64>,
@@ -8,7 +9,16 @@ pub struct Graph {
 
 impl Graph {
   pub fn new(usize point_num) -> Graph {
-      let mut points = Vec::with_capacity(point_num);
-
+      let mut points = Vec::<f64>::with_capacity(point_num);
+      let mut rng = rand::thread_rng();
+      for i in (0..point_num) {
+          points[i] = rng.gen();
+      }
+      Graph {
+          points: points,
+          polygon: vec![],
+      }
   }
+
+  pub fn output_svn() {}
 }

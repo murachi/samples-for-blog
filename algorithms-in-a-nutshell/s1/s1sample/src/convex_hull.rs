621923be3444b4bbcdca240402be765f9ee33837
Murayama Toshiyuki (T.MURACHI) <harapekomurachi@gmail.com>
2020-10-25 12:38:50 +0900

    issue #2 - sec.1 凸包計算サンプルプログラム作成 - 貪欲法まで

--- algorithms-in-a-nutshell/s1/s1sample/src/convex_hull.rs
+++ algorithms-in-a-nutshell/s1/s1sample/src/convex_hull.rs
@@ -1,6 +1,14 @@
 use std::collections::HashSet;
+use std::cmp::Ordering;
 use std::time::Instant;
 use crate::graph::Graph;
+
+#[derive(PartialEq)]
+enum RotationDirection {
+    Clockwise,
+    Anticlockwise,
+    Straight,
+}
 
 fn calc_angle(p1: (f64, f64), p2: (f64, f64), p3: (f64, f64)) -> f64 {
     let v1 = (p1.0 - p2.0, p1.1 - p2.1);
@@ -12,21 +20,27 @@ fn calc_angle(p1: (f64, f64), p2: (f64, f64), p3: (f64, f64)) -> f64 {
     theta
 }
 
-fn is_into_triangle(vt1: (f64, f64), vt2: (f64, f64), vt3: (f64, f64), pt: (f64, f64)) -> bool {
+fn calc_rotation_direction(vt1: (f64, f64), vt2: (f64, f64), vt3: (f64, f64))
+-> RotationDirection {
     let v1 = (vt2.0 - vt1.0, vt2.1 - vt1.1);
     let v2 = (vt3.0 - vt2.0, vt3.1 - vt2.1);
-    let v3 = (vt1.0 - vt3.0, vt1.1 - vt3.1);
-    let v1p = (pt.0 - vt1.0, pt.1 - vt1.1);
-    let v2p = (pt.0 - vt2.0, pt.1 - vt2.1);
-    let v3p = (pt.0 - vt3.0, pt.1 - vt3.1);
-    // 「頂点 → 頂点 → 対象点」の外積 (の Z 値) を求める
-    let outer_products_z = vec![
-        v1.0 * v1p.1 - v1.1 * v1p.0,
-        v2.0 * v2p.1 - v2.1 * v2p.0,
-        v3.0 * v3p.1 - v3.1 * v3p.0,
+    let outer_product_z = v1.0 * v2.1 - v1.1 * v2.0;
+    match outer_product_z.partial_cmp(&0.0).unwrap() {
+        Ordering::Greater => RotationDirection::Clockwise,
+        Ordering::Less => RotationDirection::Anticlockwise,
+        Ordering::Equal => RotationDirection::Straight,
+    }
+}
+
+fn is_into_triangle(vt1: (f64, f64), vt2: (f64, f64), vt3: (f64, f64), pt: (f64, f64)) -> bool {
+    let rds = [
+        calc_rotation_direction(vt1, vt2, pt),
+        calc_rotation_direction(vt2, vt3, pt),
+        calc_rotation_direction(vt3, vt1, pt),
     ];
-    // これらが全て正、または全て負であるなら、 pt は 3頂点 vt* の内部に含まれる
-    outer_products_z.iter().all(|&z| z >= 0.0) || outer_products_z.iter().all(|&z| z < 0.0)
+    // 全て時計回りか、または全て反時計回りであるなら、 pt は 3頂点 vt* の内部に含まれる
+    rds.iter().all(|rd| *rd == RotationDirection::Clockwise) ||
+    rds.iter().all(|rd| *rd == RotationDirection::Anticlockwise)
 }
 
 fn sorted_by_polar_angle(points: &Vec<(f64, f64)>) -> Vec<(f64, f64)> {
@@ -66,4 +80,30 @@ pub fn convex_hull_simple(graph: &mut Graph) {
 
     graph.polygon = polygon;
     graph.summary = format!("素朴解 - 処理時間: {}.{:03}秒", dur.as_secs(), dur.subsec_millis());
+}
+
+pub fn convex_hull_greedy(graph: &mut Graph) {
+    let now = Instant::now();
+
+    let points = sorted_by_polar_angle(&graph.points);
+    // points は先頭 pt0 (x0, y0) に対する pt (x, y) について
+    // (-20.0, y0) -> (x0, y0) -> (x, y) の内積角度の小さい順にソートしている。
+    // これを辿って描かれる凸包は、各頂点が常に反時計回りで描かれることになる。
+    let mut polygon = Vec::<(f64, f64)>::with_capacity(points.len());
+    for pt in points {
+        if polygon.len() >= 2 {
+            while calc_rotation_direction(
+                polygon[polygon.len() - 2],
+                polygon[polygon.len() - 1],
+                pt) == RotationDirection::Clockwise
+            {
+                polygon.pop();
+            }
+        }
+        polygon.push(pt);
+    }
+    let dur = now.elapsed();
+
+    graph.polygon = polygon;
+    graph.summary = format!("貪欲法 - 処理時間: {}.{:03}秒", dur.as_secs(), dur.subsec_millis());
 }
--- algorithms-in-a-nutshell/s1/s1sample/src/main.rs
+++ algorithms-in-a-nutshell/s1/s1sample/src/main.rs
@@ -4,7 +4,9 @@ pub mod convex_hull;
 use crate::graph::Graph;
 
 fn main() {
-    let mut gr = Graph::new(20_usize);
+    let mut gr = Graph::new(50);
     convex_hull::convex_hull_simple(&mut gr);
-    gr.output_svg("out.svg");
+    gr.output_svg("simple.svg");
+    convex_hull::convex_hull_greedy(&mut gr);
+    gr.output_svg("greedy.svg");
 }

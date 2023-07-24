ec60420e8696f33fb2dfaebcdd112ec1de600812
T.MURACHI (村山 俊之) <harapekomurachi@gmail.com>
2023-07-24 16:51:08 +0900

    MPEG-4 audio 出力に対応

--- python/convmp3/convmp3.py
+++ python/convmp3/convmp3.py
@@ -1,4 +1,4 @@
-import os, re
+import os, re, shutil
 from pydub import AudioSegment
 import audio_metadata
 
@@ -6,14 +6,17 @@ def convSafeName(name):
     ng_chars = r'\:*?"<>|'
     return name.translate(str.maketrans(ng_chars, "_" * len(ng_chars)))
 
-out_format = 'ogg'
-out_codec = 'libvorbis'
+out_format = 'mp4'
+out_codec = 'libfdk_aac'
+out_bitrate = '256k'
 
 inroot_dir = 'ミュージック'
-outroot_dir = out_format
+#outroot_dir = out_format
+outroot_dir = 'm4a'
 
 src_extend = '.flac'
-dist_extend = '.{}'.format(out_format)
+#dist_extend = '.{}'.format(out_format)
+dist_extend = '.m4a'
 
 default_group = 'normal'
 special_groups = {
@@ -36,6 +39,7 @@ special_groups = {
 walk = os.walk(os.path.join(os.path.expanduser('~'), inroot_dir))
 first_path, _, _ = walk.__next__()
 re_dir = re.compile(r'{}/([^/]+)(?:/(.*))?'.format(first_path))
+
 for src_path, dirs, files in walk:
     match = re_dir.match(src_path)
     artist, dir_suffix = match.groups() if match else ''
@@ -45,6 +49,8 @@ for src_path, dirs, files in walk:
         dist_path = os.path.join(dist_path, convSafeName(dir_suffix))
     print("{} -> {}".format(src_path, dist_path))
     os.makedirs(dist_path, mode = 0o755, exist_ok = True)
+
+    # 変換対象ファイルを探して変換するループ
     for src_file in [n for n in files if n.endswith(src_extend)]:
         dist_file = convSafeName(src_file.replace(src_extend, dist_extend))
         dist_fullpath = os.path.join(dist_path, dist_file)
@@ -54,6 +60,15 @@ for src_path, dirs, files in walk:
         src_fullpath = os.path.join(src_path, src_file)
         music = AudioSegment.from_file(src_fullpath, 'flac')
         metadata = audio_metadata.load(src_fullpath)
-        music.export(dist_fullpath, format = out_format, codec = out_codec,
+        music.export(dist_fullpath,
+            format = out_format, codec = out_codec, bitrate = out_bitrate,
             tags = {n[0]: ' - '.join(n[1]) for n in metadata['tags'].items()})
         #music.export(dist_fullpath, format = 'mp3', parameters = ['-ar', '44100', '-b:a', '256k'])
+
+    # 変換後の形式と同じ形式のファイルがあればそのままコピーするループ
+    for src_file in [n for n in files if n.endswith(dist_extend)]:
+        dist_fullpath = os.path.join(dist_path, src_file)
+        if os.path.exists(dist_fullpath):
+            continue
+        src_fullpath = os.path.join(src_path, src_file)
+        shutil.copy2(src_fullpath, dist_fullpath)
